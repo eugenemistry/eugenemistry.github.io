f5791e5e7be1cae262cada92b213c733736ed349
eugenemistry <booking.eugenemistry@gmail.com>
2020-05-28 16:03:31 +0300

    Forth Commit

--- WEBSITE/resources/assets/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-   $(document).scroll(function() {
-      var $navigation = $(".navigation-container");
-      $navigation.toggleClass('resized', $(this).scrollTop() > $navigation.height());
-   });
-});
